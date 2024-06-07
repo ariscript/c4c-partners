10e6340bbbacf70f35c7e819880eb9ae23e9d6e7
Ari Prakash <ariprakash16@gmail.com>
2024-06-06 22:43:23 -0400

    import starter code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+    return (
+        <>
+            <h1 className="title">C4C: Projects</h1>
+            <Dashboard />
+        </>
+    );
+}
+
+export default App;
