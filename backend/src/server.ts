9e56671a2b99980a03a527b445f668a3e689d1c9
Ari Prakash <ariprakash16@gmail.com>
2024-06-09 14:40:17 -0400

    style

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,14 @@ const partners = [
             "https://c4cneu-public.s3.us-east-2.amazonaws.com/Site/sfft-project-page.png",
         name: "Speak For The Trees",
         description:
-            "Speak for the Trees Boston aims to improve the size and health of the urban forest in the greater Boston area, with a focus on under-served and under-canopied neighborhoods. They work with volunteers to inventory (collect data) trees, plant trees, and educate those about trees. C4C has built a tree stewardship application for SFTT that allows users to participate in conserving Boston's urban forest. Across Boston, hundreds of trees have been adopted and cared for.",
+            `Speak for the Trees Boston aims to improve the size and health of
+            the urban forest in the greater Boston area, with a focus on
+            under-served and under-canopied neighborhoods. They work with
+            volunteers to inventory (collect data) trees, plant trees, and
+            educate those about trees. C4C has built a tree stewardship
+            application for SFTT that allows users to participate in conserving
+            Boston's urban forest. Across Boston, hundreds of trees have been
+            adopted and cared for.`.replaceAll(/$/gm, ""),
         active: true,
     },
 ];
@@ -28,7 +35,7 @@ app.use((_req, res, next) => {
     next();
 });
 
-app.get("/", (_req, res) => {
+app.get("/", (_, res) => {
     res.status(200).send(partners);
 });
 
